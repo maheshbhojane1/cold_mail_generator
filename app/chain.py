d3c87c078e2c23ff8a407a17a92c26720b49357b
Mahesh Bhojane <maheshbhojane200@gmail.com>
2026-04-21 10:23:13 +0530

    Fix API key for Streamlit Cloud

--- app/chain.py
+++ app/chain.py
@@ -1,5 +1,24 @@
-import os
+# import os
 
+# from langchain_groq import ChatGroq
+# from langchain_core.prompts import PromptTemplate
+# from langchain_core.output_parsers import JsonOutputParser
+# from langchain_core.exceptions import OutputParserException
+# from dotenv import load_dotenv
+
+# load_dotenv()
+
+
+# class Chain:
+#     def __init__(self):
+#         self.llm = ChatGroq(
+#             temperature=0,
+#             groq_api_key=os.getenv("GROQ_API_KEY"),
+#             model_name="llama-3.3-70b-versatile"
+#         )
+
+import os
+import streamlit as st
 from langchain_groq import ChatGroq
 from langchain_core.prompts import PromptTemplate
 from langchain_core.output_parsers import JsonOutputParser
@@ -11,9 +30,11 @@ load_dotenv()
 
 class Chain:
     def __init__(self):
+        # Works both locally and on Streamlit Cloud
+        # api_key = st.secrets.get("GROQ_API_KEY") or os.getenv("GROQ_API_KEY")
         self.llm = ChatGroq(
             temperature=0,
-            groq_api_key=os.getenv("GROQ_API_KEY"),
+            groq_api_key=os.getenv("GROQ_API_KEY") or st.secrets.get("GROQ_API_KEY") ,
             model_name="llama-3.3-70b-versatile"
         )
 
